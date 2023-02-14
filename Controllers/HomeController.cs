63802b9bcd17dc18568272b87bd6950717f88f64
NaviteLogger <kacprzakmarek92@gmail.com>
2023-02-14 18:31:50 +0100

    Modified the Program.cs class

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,30 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
 
-namespace PartyInvites.Controllers;
-
-public class HomeController : Controller
+namespace PartyInvites.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
