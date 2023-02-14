af018ce15924b27345434974aafe0e15d5313094
NaviteLogger <kacprzakmarek92@gmail.com>
2023-02-14 19:16:22 +0100

    Added comments to the HomeController.cs class

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,11 +4,17 @@ using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller //you can define a controller by deriving from the Controller class
+                                             //you can define multiple controllers in a single file
     {
         public IActionResult Index()
         {
-            return View();
+            return View(); //Razor engine will use the Index.cshtml file to generate the response
+        }
+
+        public ViewResult RsvpForm() //defines the method that will handle the received request
+        { 
+            return View(); //Razor engine will use the RsvpForm.cshtml file to generate the response
         }
     }
 }
