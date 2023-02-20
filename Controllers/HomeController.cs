58a77478b071bcf26435211dd7fe4f065cd69573
NaviteLogger <kacprzakmarek92@gmail.com>
2023-02-20 18:43:38 +0100

    Started working on processing the guest's form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,9 +12,16 @@ namespace PartyInvites.Controllers
             return View(); //Razor engine will use the Index.cshtml file to generate the response
         }
 
+        [HttpGet]
         public ViewResult RsvpForm() //defines the method that will handle the received request
         { 
             return View(); //Razor engine will use the RsvpForm.cshtml file to generate the response
+        }
+
+        [HttpPost] //This version of the action will be responsible for receiving submitted data and deciding what to do with it.
+        public ViewResult RsvpForm(GuestResponse guestResponse) //defines the method that will handle the received request
+        {
+            return View();
         }
     }
 }
