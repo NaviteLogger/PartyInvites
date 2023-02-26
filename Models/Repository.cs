df662edfb4ab612d82fe3319da3966f0d3f7ad98
NaviteLogger <kacprzakmarek92@gmail.com>
2023-02-26 22:30:45 +0100

    Added comments to Repository.cs class

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -11,3 +11,12 @@
         }
     }
 }
+
+/* I am going to do this by creating an in-memory 
+ * collection of objects.This isn’t useful in a 
+ * real application because the response data will
+ * be lost when the application is stopped or restarted,
+ * but this approach will allow me to keep the focus on
+ * ASP.NET Core and create an application that can easily 
+ * be reset to its initial state.
+ */
